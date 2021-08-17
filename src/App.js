7fa78edd8bf3e29eb48c7cc80522173c16b8cecf
maria_adelia <maria.adelia.contato@gmail.com>
2021-08-17 14:09:53 -0300

    Desafio React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import { Container, Form, Jumbotron, Row } from 'react-bootstrap';
+import { Container, Jumbotron } from 'react-bootstrap';
 import React from 'react';
 import Componente from './Componente';
 
 function App() {
-  const [textoBusca, setTextoBusca] = React.useState("");
-  const textoBuscaMudou = event => setTextoBusca(event.target.value);
-
+  
   return (
     <Container className="p-3">
       <Jumbotron>
         <h1 className="header">Doce Sonho</h1>
         <Container>
-        <Row>
-            <Form>
-              <Form.Group className="mb-3">
-                <Form.Control type="text" value={textoBusca} onChange={textoBuscaMudou} placeholder="Buscar por produto..." />
 
-              </Form.Group>
-            </Form>
-          </Row>
-        <Componente></Componente>
+        {/** Tag responsável pelos componentes (barra de pesquisa e produtos) */}  
+        <Componente>
+        </Componente>
+        {/** /Tag responsável pelos componentes (barra de pesquisa e produtos) */} 
+        
         </Container>
       </Jumbotron>
     </Container>
--- src/Componente.js
+++ src/Componente.js
@@ -1,7 +1,9 @@
-import { Button, Card, Row } from 'react-bootstrap';
+import { Button, Card, Container, Row, Form } from 'react-bootstrap';
 import React from 'react';
 
 function ComponenteCard() {
+
+    //Produtos cadastrados
     const produtos = React.useMemo(() => [
         {
             nome: 'Bombom ao Leite',
@@ -26,19 +28,46 @@ function ComponenteCard() {
         },
     ], []);
 
+    //Constantes para a barra de pesquisa
+    const [textoBusca, setTextoBusca] = React.useState("");
+    const textoBuscaMudou = event => setTextoBusca(event.target.value);
+
+    //Barra de Pesquisa e os Cards de Produto
     return (
-        <Row>
-            {produtos.map((d,i) =>
-            <Card key={i} className="m-2" style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={d.imagem} />
-                <Card.Body>
-                    <Card.Title>{d.nome}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">{d.preco}</Card.Subtitle>
-                    <Card.Text>{d.descricao}</Card.Text>
-                    <Button variant="primary">Visualizar Produto</Button>
-                </Card.Body>
-            </Card>)}
-        </Row>
+        <Container>
+            <Row>
+                {/*Barra de pesquisa */}
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Control type="text" value={textoBusca} onChange={textoBuscaMudou} placeholder="Buscar por produto..." />
+                    </Form.Group>
+                </Form>
+
+            </Row>
+            <Row>
+                {/*Lógica da barra de pesquisa e dos cards */}
+                {produtos
+                    .filter((d) => {
+                        if (textoBusca === "") {
+                            return d
+                        }
+                        else if (d.nome.toLowerCase().includes(textoBusca.toLowerCase())) {
+                            return d
+                        }
+                    })
+                    .map((d, i) =>
+                        <Card key={i} className="m-2" style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={d.imagem} />
+                            <Card.Body>
+                                <Card.Title>{d.nome}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">{d.preco}</Card.Subtitle>
+                                <Card.Text>{d.descricao}</Card.Text>
+                                <Button variant="primary">Visualizar Produto</Button>
+                            </Card.Body>
+                        </Card>)}
+            </Row>
+        </Container>
+
     );
 }
 
